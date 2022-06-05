6a1800c267f002394f4cee392d43a9c313af438c
morganales <gaby.leite2008@gmail.com>
2022-06-05 16:13:44 -0300

    first-java-script

---
+++ index.js
@@ -0,0 +1,14 @@
+const primeiroNome = "Gabriela"
+const ultimoNome = "Leite"
+const idade = 27;
+
+console.log(primeiroNome);
+console.log(ultimoNome);
+console.log(idade);
+
+console.log(primeiroNome; + ultimoNome; + idade;);
+
+
+
+
+
