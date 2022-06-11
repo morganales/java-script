d8cc4c2a829cffec1402795d7271fb675efb6d08
morganales <gaby.leite2008@gmail.com>
2022-06-11 20:22:26 -0300

    Atividade pratica 2

---
+++ atividades-praticas/exercicio-pratico2.js
@@ -0,0 +1,6 @@
+const distanciaPercorrida = 30; //
+const tempoGasto = 40;
+
+const velocidadeMedia = distanciaPercorrida / tempoGasto;
+console.log(`A velocidade média é igual a ${velocidadeMedia}`);
+
