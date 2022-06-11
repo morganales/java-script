6abb6cad7afa3ca70b784564b5970d09a8f3b8c1
morganales <gaby.leite2008@gmail.com>
2022-06-11 20:12:38 -0300

    atividades praticas Minas Programam

---
+++ atividades-praticas/exercicio-pratico1.js
@@ -0,0 +1,12 @@
+var nome = "Gabriela";
+var idade = 26;
+var altura = 1.55;
+var entregouAtrasado = "Sim";
+
+
+if (entregouAtrasado = "Sim"){
+    console.log(`Meu nome é ${nome} eu tenho ${idade} anos, ${altura} de altura e eu entreguei a atividade atrasada.`);
+} else if (entregouAtrasado = "Não"){
+    console.log(`Meu nome é ${nome} eu tenho ${idade} anos e ${altura} de altura e eu NÃO entreguei a atividade atrasada.`);
+}
+
